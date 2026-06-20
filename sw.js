4210c1aeecc2e45130e88584982440f9f1113209
foodiespoint <prime.aksh@gmail.com>
2026-06-20 20:48:54 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // FOODIES POINT - SERVICE WORKER (PRODUCTION ENGINE V11)
 // ==========================================================================
 
-const CACHE_NAME = 'foodies-cache-v13';
+const CACHE_NAME = 'foodies-cache-v14';
 
 const ASSETS = [
   '',
